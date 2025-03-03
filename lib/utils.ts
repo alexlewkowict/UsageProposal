d252cdd88b407df45702ee0f9b877c7032b34e69
alexlewkowict <Alex.lewkowict@shiphero.com>
2025-03-03 14:36:28 -0500

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,3 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+export const formatNumber = (value: number): string => {
+  return value.toLocaleString('en-US');
+};
+
